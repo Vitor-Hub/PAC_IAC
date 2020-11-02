dc8f5b4cee0e41426bce587a3ebbd2be13c6ca16
Vitor Pereira <vitor.pereira@inpartec.com.br>
2020-11-02 13:59:00 -0300

    changes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '../css/index.css';
+import '../../css/index.css';
+import logo from '../../css/images/logo.png'
+import { DropDownButtonComponent } from '@syncfusion/ej2-react-splitbuttons';
+import { DropDownListComponent } from '@syncfusion/ej2-react-dropdowns';
 
 const Header = () => {
-    
+
+    const listData=[
+        {
+            text1: 'teste1'
+        }
+    ]
+
+    const itemData = () => {
+        return(
+            <span>
+                {listData.text1}
+            </span>
+        )
+    }
+
+    return (
+        <header id="header">
+            <div>
+                <img src={logo} />
+                <DropDownListComponent id="dropDown" dataSource={listData} itemTemplate={itemData} width= "100px" popupHeight="200px" popupWidth="250px" placeholder="Meu Perfil" />
+            </div>
+        </header>
+    )
 }
 
 export default Header;
---
+++ src/pages/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../css/index.css';
+
+import Header from '../components/header/Header';
+
+const Main = () => {
+    return(
+        <Header/>
+    )
+}
+
+export default Main;
